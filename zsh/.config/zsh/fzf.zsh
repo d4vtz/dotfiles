e61a179ff4f8085283a765b9e12ff0fee54250fd
D4vtz <davidtorrezreyes@gmail.com>
2026-08-27 23:16:21 -0600

    zsh: Agregando configuraciones para fd

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -53,5 +53,12 @@ export FZF_COMPLETION_OPTS="
     --info=inline-right
 "
 
+# Use fd as the source for fzf
+if (( $+commands[fd] )); then
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+    export FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
+fi
+
 # Official Zsh integration: completion and key bindings
 source <(fzf --zsh)
